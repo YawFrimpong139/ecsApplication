eff12060026c71be6da0215015ae84ebe94d369d
Yaw Frimpong Yeboah <165104209+YawFrimpong139@users.noreply.github.com>
2025-09-24 11:33:38 +0000

    Add My to the lab

--- src/main/java/org/codewithzea/ecsapplication/controller/HomeController.java
+++ src/main/java/org/codewithzea/ecsapplication/controller/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("name", "Yaw Frimpong");
-        model.addAttribute("lab", "ECS Deployment Lab");
+        model.addAttribute("lab", "My ECS Deployment Lab");
         model.addAttribute("description", "ECS Deployment Lab Description");
         model.addAttribute("desc","This is a simple Spring Boot application deployed on AWS ECS using Fargate.");
         return "index";
